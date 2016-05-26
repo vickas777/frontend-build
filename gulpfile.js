a001a4ce516f4356535a18141acc3ce0e2715809
Bogdan Keller <Bogdan Keller>
2016-05-26 22:44:04 +0300

    Add error notify to all tasks, fix watcher task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 const isDev = process.env.NODE_ENV === 'dev' || !process.env.NODE_ENV;
 
 const gulp = require('gulp');
-const watch = require('gulp-watch');
 const prefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
@@ -16,6 +15,7 @@ const rimraf = require('rimraf');
 const browserSync = require("browser-sync");
 const gulpIf = require('gulp-if');
 const notify = require('gulp-notify');
+const combine = require('stream-combiner2').obj;
 const reload = browserSync.reload;
 
 let path = {
@@ -53,72 +53,90 @@ let config = {
 };
 
 gulp.task('html:build', function () {
-  return gulp.src(path.src.html)
-    .pipe(rigger())
-    .pipe(gulp.dest(path.build.html))
-    .pipe(reload({stream: true}));
+  return combine(
+    gulp.src(path.src.html),
+    rigger(),
+    gulp.dest(path.build.html),
+    reload({stream: true})
+  ).on('error', notify.onError(function (err) {
+    return {
+      title: "HTML",
+      message: err.message
+    };
+  }));
 });
 
 gulp.task('js:build', function () {
-  return gulp.src(path.src.js)
-    .pipe(gulpIf(isDev, sourcemaps.init()))
-    .pipe(rigger())
-    .pipe(uglify())
-    .pipe(gulpIf(isDev, sourcemaps.write()))
-    .pipe(gulp.dest(path.build.js))
-    .pipe(reload({stream: true}));
+  return combine(
+    gulp.src(path.src.js),
+    gulpIf(isDev, sourcemaps.init()),
+    rigger(),
+    uglify(),
+    gulpIf(isDev, sourcemaps.write()),
+    gulp.dest(path.build.js),
+    reload({stream: true})
+  ).on('error', notify.onError(function (err) {
+    return {
+      title: "JavaScript",
+      message: err.message
+    };
+  }));
 });
 
 gulp.task('style:build', function () {
-  return gulp.src(path.src.style)
-    .pipe(gulpIf(isDev, sourcemaps.init()))
-    .pipe(sass())
-    .on('error', notify.onError(function (err) {
-      return {
-        title: "Styles",
-        message: err.message
-      };
-    }))
-    .pipe(prefixer())
-    .pipe(cssmin())
-    .pipe(gulpIf(isDev, sourcemaps.write()))
-    .pipe(gulp.dest(path.build.css))
-    .pipe(reload({stream: true}));
+  return combine(
+    gulp.src(path.src.style),
+    gulpIf(isDev, sourcemaps.init()),
+    sass(),
+    prefixer(),
+    cssmin(),
+    gulpIf(isDev, sourcemaps.write()),
+    gulp.dest(path.build.css),
+    reload({stream: true})
+  ).on('error', notify.onError(function (err) {
+    return {
+      title: "Styles",
+      message: err.message
+    };
+  }));
 });
 
 gulp.task('image:build', function () {
-  return gulp.src(path.src.img)
-    .pipe(imagemin({
+  return combine(
+    gulp.src(path.src.img),
+    imagemin({
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()],
       interlaced: true
-    }))
-    .pipe(gulp.dest(path.build.img))
-    .pipe(reload({stream: true}));
+    }),
+    gulp.dest(path.build.img),
+    reload({stream: true})
+  ).on('error', notify.onError(function (err) {
+    return {
+      title: "Images",
+      message: err.message
+    };
+  }));
 });
 
-gulp.task('fonts:build', function() {
+gulp.task('fonts:build', function () {
   return gulp.src(path.src.fonts)
-    .pipe(gulp.dest(path.build.fonts));
+    .pipe(gulp.dest(path.build.fonts))
+    .on('error', notify.onError(function (err) {
+      return {
+        title: "Fonts",
+        message: err.message
+      };
+    }));
 });
 
-gulp.task('watch', function(){
-  watch([path.watch.html], function(event, cb) {
-    gulp.start('html:build');
-  });
-  watch([path.watch.style], function(event, cb) {
-    gulp.start('style:build');
-  });
-  watch([path.watch.js], function(event, cb) {
-    gulp.start('js:build');
-  });
-  watch([path.watch.img], function(event, cb) {
-    gulp.start('image:build');
-  });
-  watch([path.watch.fonts], function(event, cb) {
-    gulp.start('fonts:build');
-  });
+gulp.task('watch', function () {
+  gulp.watch(path.watch.html, gulp.series('html:build'));
+  gulp.watch(path.watch.style, gulp.series('style:build'));
+  gulp.watch(path.watch.js, gulp.series('js:build'));
+  gulp.watch(path.watch.img, gulp.series('image:build'));
+  gulp.watch(path.watch.fonts, gulp.series('fonts:build'));
 });
 
 gulp.task('webserver', function () {
@@ -129,11 +147,8 @@ gulp.task('clean', function (cb) {
   rimraf(path.clean, cb);
 });
 
-gulp.task('build', gulp.parallel( 'html:build',
-                                  'js:build',
-                                  'style:build',
-                                  'fonts:build',
-                                  'image:build')
-);
 
-gulp.task('default', gulp.series('clean', 'build', gulp.parallel('webserver', 'watch')));
+gulp.task('default', gulp.series('clean',
+  gulp.parallel( 'html:build', 'js:build', 'style:build', 'fonts:build', 'image:build' ),
+  gulp.parallel('watch', 'webserver'))
+);
